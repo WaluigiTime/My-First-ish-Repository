bd7d5b4a487195066f35c09929b07cee31c67593
WaluigiTime <43816974+WaluigiTime@users.noreply.github.com>
2021-10-13 18:30:10 -0400

    made a cool-ish about page shoutouts to sami and emily for carrying

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import "./styles.css";
 
 export default function App() {
+  const omari = "Omari";
   return (
     <div className="App">
-      <h1>hey what's up gamers</h1>
-      <h2>Year: 2021 High School: P-Tech</h2>
+      <div>
+        <h1>hey what's up gamers</h1>
+        <h2>Year: 2021 High School: P-Tech</h2>
+        <h3>{omari}</h3>
+      </div>
+
+      <div className="About Me">
+        hi my name is omari this is my about page
+        <img
+          src="https://www.imore.com/sites/imore.com/files/styles/xlarge_wm_brw/public/field/image/2021/07/nintendo-switch-oled-model-with-tv.jpg"
+          alt="nintendo"
+        />
+      </div>
     </div>
   );
 }
